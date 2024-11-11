ef72d6a328f4e5668201bf3f6fa7580fc47ca54a
Arttu Kaitila <arttu.kaitila@gmail.com>
2024-11-11 11:48:02 +0200

    prepared for switching rand algs

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,8 +1,24 @@
 export default function random(seed: string) {
   const hash = cyrb128(seed);
+  //return sfc32(hash[0], hash[1], hash[2], hash[3])
   return splitmix32(hash[0]);
 }
 
+function sfc32(a: number, b: number, c: number, d: number) {
+  return function () {
+    a |= 0;
+    b |= 0;
+    c |= 0;
+    d |= 0;
+    let t = (((a + b) | 0) + d) | 0;
+    d = (d + 1) | 0;
+    a = b ^ (b >>> 9);
+    b = (c + (c << 3)) | 0;
+    c = (c << 21) | (c >>> 11);
+    c = (c + t) | 0;
+    return (t >>> 0) / 4294967296;
+  };
+}
 function splitmix32(a: number) {
   return function () {
     a |= 0;
