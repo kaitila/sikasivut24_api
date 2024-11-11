6f849be812ad327f70b4aa53cd3c734a4feb8519
Arttu Kaitila <arttu.kaitila@gmail.com>
2024-11-11 14:15:06 +0200

    updated daily ticket limit

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 export const POINTS_RATE = 50;
 export const MIN_POINTS_THRESHOLD = 4000;
 export const TICKET_ODDS = 0.03;
-export const DAILY_TICKET_LIMIT = 3;
+export const DAILY_TICKET_LIMIT = 6;
 export const SENDER_EMAIL = "MS_TKEasB@trial-z3m5jgr2kjdldpyo.mlsender.net";
 export const EMAIL_SUBJECT = "Drinkkilippu";
